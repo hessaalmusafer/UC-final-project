2e89503190383281557326aab058eb044275d533
hessaalmusafer <hessaalmusafer@gmail.com>
2022-09-22 01:44:05 +0300

    1

---
+++ FinalProject/FinalProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FinalProject
+//
+//  Created by Hessa AlMusafer on 22/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FinalProject/FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by Hessa AlMusafer on 22/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
